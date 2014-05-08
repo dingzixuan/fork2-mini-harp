1c01a87a2d67517235096cb304e84b15f061e377
dingzixuan <dingzixuan90@gmail.com>
2014-05-08 22:27:13 +0800

    implement current time middleware & serving static content

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 var argv = require("minimist")(process.argv.slice(2));
 var createMiniHarp = require("mini-harp");
-var app = createMiniHarp(argv.port);
+var app = createMiniHarp(argv.port, argv._[0]);
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 var connect = require("connect");
+var serveStatic = require("serve-static");
 var app = connect();
-function createMiniHarp(port) {
+
+function createMiniHarp(port, root) {
   if (!port) {
     port = 4000;    
   }
+  if (!root) {
+    root = process.cwd();
+  }
+  app.use(serveStatic(root));
+  app.use(function(req, res, next) {
+    //console.log(req.url);
+    if(req.url == "/current-time") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end((new Date()).toISOString());
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Cannot Get " + req.url);
+    }
+  });
   app.listen(port);
   console.log("Starting mini-harp on http://localhost:" + port);
   return app;
